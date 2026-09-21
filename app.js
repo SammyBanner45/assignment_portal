70c42ad0ff497cdce7df12dc7ae34039f5675cca
SammyBanner45 <sambuddha.banerjee45@gmail.com>
2026-09-21 10:26:15 +0530

    patch method added

--- app.js
+++ app.js
@@ -54,6 +54,31 @@ app.get("/assignments", async (req, res) => {
   }
 });
 
+// 3. MARK ASSIGNMENT AS SUBMITTED
+app.patch("/assignments/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `UPDATE assignments
+       SET submitted = true
+       WHERE id = $1
+       RETURNING *`,
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Assignment not found",
+      });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
