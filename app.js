fc7c21895dd9ed59b7631a564e0e508be678fcf0
SammyBanner45 <sambuddha.banerjee45@gmail.com>
2026-09-21 10:30:19 +0530

    delete method added

--- app.js
+++ app.js
@@ -80,6 +80,34 @@ app.patch("/assignments/:id", async (req, res) => {
   }
 });
 
+// 4. DELETE ASSIGNMENT
+app.delete("/assignments/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `DELETE FROM assignments
+       WHERE id = $1
+       RETURNING *`,
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "Assignment not found",
+      });
+    }
+
+    res.json({
+      message: "Assignment deleted successfully",
+      assignment: result.rows[0],
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
